9dba595ae4ffaa63a49eab7f2597e48ce3c3023d
Njihia Maureen <maureennjihia468@gmail.com>
2024-03-07 18:25:04 +0300

    Attempting navbar

--- echosign/tailwind.config.js
+++ echosign/tailwind.config.js
@@ -1,13 +1,20 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withMT( {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
         'poppins': ['"Poppins"', 'sans-serif'],
       },
+      backgroundImage: {
+        'brownBackground': "url('../public/brownBg.png')"
+      }
     },
   },
-  plugins: [],
-}
+  plugins: [
+    require('flowbite/plugin')
+  ],
+});
 
